eb1f88e7d83a89114a285fbce05c69cce7d68b03
x2lev <lev.kryvenko@proton.me>
2025-07-07 01:34:38 -0600

    updated fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    fastfetch
+    cat ~/.cache/wal/sequences
+    abbr -a vimf "vim ~/.config/fish/config.fish"
+    abbr -a vimh "vim ~/.config/hypr/hyprland.conf"
+    abbr -a vimwc "vim ~/.config/waybar/config.jsonc"
+    abbr -a vimws "vim ~/.config/waybar/style.css"
+    set fish_greeting
+end
+
+source /etc/environment
