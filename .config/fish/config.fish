472dd9e3640acf363f79f0375a234a107f0e3a84
x2lev <lev.kryvenko@proton.me>
2025-07-16 15:35:01 -0600

    routine update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     #cat ~/.cache/wal/sequences
-    fastfetch
-    abbr -a vimf "vim ~/.config/fish/config.fish"
-    abbr -a vimh "vim ~/.config/hypr/hyprland.conf"
-    abbr -a vimwc "vim ~/.config/waybar/config.jsonc"
-    abbr -a vimws "vim ~/.config/waybar/style.css"
+    if [ $TERM = "xterm-kitty" ]
+        fastfetch
+    end
+    abbr -a naf "nano ~/.config/fish/config.fish"
+    abbr -a nah "nano ~/.config/hypr/hyprland.conf"
+    abbr -a nac "nano ~/.config/waybar/config.jsonc"
+    abbr -a nas "nano ~/.config/waybar/style.css"
     set fish_greeting
 end
 
